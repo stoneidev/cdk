6812ecd4a2c17b8a564704db79016d2cad11fe8f
Kim Jong IL <jngkim@amazon.com>
2024-07-21 10:20:21 +0900

    send mail service

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,3 +1,8 @@
+const AWS = require("aws-sdk");
+
+// SES 설정
+const ses = new AWS.SES({ region: "ap-northeast-2" });
+
 exports.handler = async function (event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
 
@@ -18,6 +23,8 @@ exports.handler = async function (event, context) {
     };
   }
 
+  await sendMail();
+
   // 실제 요청 처리
   return {
     statusCode: 200,
@@ -29,4 +36,34 @@ exports.handler = async function (event, context) {
       message: `Hello, CDK! You've hit ${event.path}`,
     }),
   };
+};
+
+sendMail = async () => {
+  const params = {
+    Destination: {
+      ToAddresses: ["jngkim@amazon.com"], // 수신자 이메일 주소
+    },
+    Message: {
+      Body: {
+        Text: { Data: "This is a test email from AWS Lambda using SES." }, // 이메일 본문
+      },
+      Subject: { Data: "Test Email" }, // 이메일 제목
+    },
+    Source: "jngkim@amazon.com", // 발신자 이메일 주소
+  };
+
+  try {
+    const data = await ses.sendEmail(params).promise();
+    console.log("Email sent successfully:", data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify("Email sent successfully!"),
+    };
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify("Failed to send email."),
+    };
+  }
 };
